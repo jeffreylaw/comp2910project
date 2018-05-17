7ae1474082a4c937bdec841d86155bdd87ba08d0
Ryan Cheong <rhhjjeong@hotmail.com>
2018-05-16 19:04:47 -0700

    added score 3000 ending

--- js/index.js
+++ js/index.js
@@ -414,7 +414,378 @@ function startTimer() {
     }, 1000);
 }
 
+function updateTapPage() {
+    var taps = document.getElementsByClassName("tap");
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+    var tapHeight = height * 0.19;
+    var tapWidth = width * 0.19;
+    for (let i = 0; i < taps.length; i++) {
+        if (width > height) {
+            taps[i].style.width = "auto";
+            taps[i].style.height = "19vh";
+        } else {
+            taps[i].style.height = "auto";
+            taps[i].style.width = "19%";
+        }
+    }
+    if (width > height) {
+        $("#tap2").css("left", (width / 2 - tapHeight / 2) + "px");
+        $("#tap5").css("left", (width / 2 - tapHeight / 2) + "px");
+        $("#tap8").css("left", (width / 2 - tapHeight / 2) + "px");
+    } else {
+        $("#tap2").css("left", (width / 2 - tapWidth / 2) + "px");
+        $("#tap5").css("left", (width / 2 - tapWidth / 2) + "px");
+        $("#tap8").css("left", (width / 2 - tapWidth / 2) + "px");
+    }
+    
+    setTimeout(updateTapPage, 10);
+}
 
+
+//--------------------------------------------Mini Game3--------------------------------------------//
+//--------------------------------------------Mini Game3--------------------------------------------//
+//--------------------------------------------Mini Game3--------------------------------------------//
+//--------------------------------------------Mini Game3--------------------------------------------//
+//--------------------------------------------Mini Game3--------------------------------------------//
+
+var objects = [];
+var touch2 = false;
+var myScore;
+var collected = 0;
+var gravity = 0.05;
+var numLives;
+var mouseClicked;
+var lives = 3;
+var score = 0;
+var sliceGameMusic;
+var sliceGameStopped = false;
+
+var shower = {
+    x: Math.random() * width,
+    y: height * 0.7,
+    width: width * 0.1,
+    height: width * 0.1,
+    src: "./images/shower.png",
+    speed: 15,
+    score: 100,
+    life: 0,
+    hitTop: false,
+    dropped: -1
+};
+
+var faucet = {
+    x: Math.random() * width,
+    y: height * 0.7,
+    width: width * 0.1,
+    height: width * 0.1,
+    src: "./images/faucet.png",
+    speed: 15,
+    score: 100,
+    life: 0,
+    hitTop: false,
+    dropped: -1
+};
+var washer = {
+    x: Math.random() * width,
+    y: height * 0.7,
+    width: width * 0.1,
+    height: width * 0.1,
+    src: "./images/washer.png",
+    speed: 15,
+    score: 100,
+    life: 0,
+    hitTop: false,
+    dropped: -1
+};
+
+var waterBottle = {
+    x: Math.random() * width,
+    y: height * 0.7,
+    width: width * 0.1,
+    height: height * 0.1,
+    src: "./images/waterBottle.png",
+    speed: 15,
+    score: -100,
+    life: -1,
+    hitTop: false,
+    dropped: 0
+};
+
+window.addEventListener("mousedown", mouseDown2, false);
+window.addEventListener("mousemove", mouseMove2, false);
+window.addEventListener("mouseup", mouseUp2, false);
+
+window.addEventListener('touchstart', touchStart2, false);
+window.addEventListener('touchmove', touchMove2, false)
+window.addEventListener('touchend', touchEnd2, false);
+
+function mouseDown2(e) {
+    touch2 = true;
+}
+
+function mouseMove2(e) {
+    if (touch2 === true && e.pageX <= gameArea2.canvas.width
+        && e.pageX >= 0 && e.pageY <= gameArea2.canvas.height && e.pageY >= 0) {
+        gameArea2.x = e.pageX;
+        gameArea2.y = e.pageY;
+        console.log("what");
+    } else {
+        gameArea2.x = 0;
+        gameArea2.y = 0;
+    }
+}
+
+function mouseUp2(e) {
+    console.log("touchend");
+    touch2 = false;
+    gameArea2.x = 0;
+    gameArea2.y = 0;
+}
+
+
+function touchStart2(e) {
+    console.log("2touched");
+    touch2 = true;
+}
+
+function touchMove2(e) {
+    if (touch2 === true && e.touches[0].screenX < gameArea.canvas.width) {
+        var touchLocation = e.targetTouches[0];
+        gameArea2.x = touchLocation.pageX;
+        gameArea2.y = touchLocation.pageY;
+        console.log("2what");
+    } else {
+        gameArea2.x = 0;
+        gameArea2.y = 0;
+    }
+}
+
+function touchEnd2(e) {
+    console.log("2touchend");
+    touch2 = false;
+    gameArea2.x = 0;
+    gameArea2.y = 0;
+}
+
+function startSliceGame() {
+    gameArea2.start();
+    //sliceGameMusic = new sound("sliceGame.mp3");
+    myScore = new componentText2(height * 0.05 + "px", "Consolas", "black", 0, height * 0.05, "text");
+    numLives = new componentText2(height * 0.05 + "px", "Consolas", "black", width * 0.8, height * 0.05, "text");
+}
+
+var gameArea2 = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        gameArea2.canvas = document.createElement("canvas");
+        gameArea2.canvas.setAttribute("id", "sliceCanvas");
+        this.canvas.width = width;
+        this.canvas.height = height * 0.7;
+        // Adds context
+        this.context = this.canvas.getContext("2d");
+        let div1 = document.getElementById("sliceGame");
+        let div2 = document.getElementById("sliceDIV");
+        div1.appendChild(this.canvas);
+        div2.style.display = "block";
+        this.interval = setInterval(updateGameArea2, 20);
+        this.frameNum = 0;
+
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function () {
+        sliceGameStopped = true;
+        window.removeEventListener('mousemove', mouseMove2);
+        window.removeEventListener('mousedown', mouseDown2);
+        window.removeEventListener('mouseup', mouseUp2);
+        window.removeEventListener('touchstart', touchStart);
+        window.removeEventListener('touchmove', touchMove);
+        window.removeEventListener('touchend', touchEnd);
+
+        /*         clearInterval(this.interval);
+                window.removeEventListener('mousemove', mouseMove);
+                window.removeEventListener('touchstart', touchStart);
+                window.removeEventListener('touchmove', touchMove)
+                window.removeEventListener('touchend', touchEnd); */
+    }
+}
+
+function componentText2(fontSize, fontType, color, x, y, text) {
+    this.fontSize = fontSize;
+    this.fontType = fontType;
+    this.x = x;
+    this.y = y;
+    this.update = function () {
+        obj1 = gameArea2.context;
+        obj1.font = this.fontSize + " " + this.fontType;
+        obj1.fillStyle = color;
+        obj1.fillText(this.text, this.x, this.y);
+    }
+
+}
+
+function newGameObject(obj) {
+    this.width = obj.width;
+    this.height = obj.height;
+    this.x = obj.x;
+    this.y = obj.y;
+    this.src = obj.src;
+    this.score = obj.score;
+    this.life = obj.life;
+    this.dropped = obj.dropped;
+    this.hitTop = false;
+    this.hitLeft = false;
+    this.hitRight = false;
+    this.speed = height * 0.01;
+    this.direction = Math.floor(Math.random() * 2);
+    this.update = function () {
+        obj1 = gameArea2.context;
+        obj1 = new Image(this.width, this.height);
+        obj1.src = this.src;
+        gameArea2.context.drawImage(obj1, this.x, this.y, this.width, this.height);
+    }
+
+    this.sliced = function () {
+        let myleft = this.x;
+        let myright = this.x + (this.width);
+        let mytop = this.y;
+        let mybottom = this.y + (this.height);
+        let collide = false;
+        if (gameArea2.x >= myleft && gameArea2.x <= myright
+            && gameArea2.y >= mytop && gameArea2.y <= mybottom) {
+            collide = true;
+        }
+        return collide;
+        console.log(collide);
+    }
+
+}
+
+function addObjectOntoScreen() {
+    let x = Math.floor(Math.random() * (gameArea2.canvas.width));
+    let y = 1;
+    let randCase = Math.floor(Math.random() * 4);
+    switch (randCase) {
+        case 0:
+            objects.push(new newGameObject(waterBottle));
+            objects[objects.length - 1].x = x;
+            break;
+        case 1:
+            objects.push(new newGameObject(washer));
+            objects[objects.length - 1].x = x;
+            break;
+        case 2:
+            objects.push(new newGameObject(faucet));
+            objects[objects.length - 1].x = x;
+            break;
+        case 3:
+            objects.push(new newGameObject(shower));
+            objects[objects.length - 1].x = x;
+            break;
+        default:
+            break;
+    }
+}
+
+function updateGameArea2() {
+    width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+    gameArea2.canvas.width = width;
+    gameArea2.canvas.height = height * 0.7;
+    var x, y;
+
+    gameArea2.clear();
+    if (sliceGameStopped === false) {
+        gameArea2.frameNum += 1;
+    }
+    var rand = Math.floor(Math.random() * (120 - 50)) + 50;
+    if (gameArea2.frameNum === 1 || everyinterval2(rand) && sliceGameStopped === false) {
+        addObjectOntoScreen();
+        //console.log(objects);
+    }
+    if (everyinterval2(80) && sliceGameStopped === false) {
+        addObjectOntoScreen();
+    }
+
+    for (let i = objects.length - 1; i >= 0; i--) {
+
+        objects[i].width = width * 0.1;
+        objects[i].height = width * 0.1;
+
+        if (sliceGameStopped === false) {
+            if (objects[i].x <= 0) {
+                objects[i].x = objects[i].x + 1;
+                objects[i].hitLeft = true;
+            } else if (objects[i].x >= width - objects[i].width) {
+                objects[i].x = objects[i].x - 1;
+                objects[i].hitRight = true;
+            } else if (objects[i].hitLeft == true) {
+                objects[i].x = objects[i].x + 1;
+            } else if (objects[i].hitRight == true) {
+                objects[i].x = objects[i].x - 1;
+            } else {
+                switch (objects[i].direction) {
+                    case 0:
+                        objects[i].x = objects[i].x + 1;
+                        break;
+                    case 1:
+                        objects[i].x = objects[i].x - 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (objects[i].y <= height * 0.05) {
+                objects[i].hitTop = true;
+                objects[i].speed = 0;
+
+            }
+            if (objects[i].hitTop == true) {
+                objects[i].speed += gravity;
+                objects[i].y += 1 + objects[i].speed;
+                //console.log(objects[i].speed);
+            } else {
+                objects[i].speed -= height * 0.0001;
+                objects[i].y -= 1 + objects[i].speed;
+            }
+        }
+
+        objects[i].update();
+
+        if (sliceGameStopped === false) {
+            if (objects[i].y >= height * 0.7) {
+                lives = lives + objects[i].dropped;
+                objects.splice(i, 1);
+                //console.log(objects);
+            } else if (objects[i].sliced()) {
+                score = score + objects[i].score;
+                lives = lives + objects[i].life;
+                objects.splice(i, 1);
+
+            }
+            if (lives <= 0 || score == 3000) {
+                $('#next').show(0);
+                gameArea2.stop();
+            }
+        }
+    }
+
+    myScore.text = "Score: " + score;
+    myScore.fontSize = width * 0.03 + "px";
+    numLives.fontSize = width * 0.03 + "px";
+    numLives.x = width * 0.8;
+    numLives.text = "Lives: " + lives;
+    numLives.update();
+    myScore.update();
+}
+
+function everyinterval2(n) {
+    if ((gameArea2.frameNum / n) % 1 == 0) { return true; }
+    return false;
+}
 
 //----------------------------------------------story-----------------------------------------------//
 //----------------------------------------------story-----------------------------------------------//
@@ -479,6 +850,8 @@ $(document).ready(function () {
     $('#beginningPage').hide(0);
     $('#text1').hide(0);
     $('#next').hide(0);
+    $('#sliceDIV').hide(0);
+    $('#sliceGame').hide(0);
 
     //for mini game2---------------------------------------------------------------------------------
 
@@ -579,6 +952,7 @@ $(document).ready(function () {
         switch (textNum) {
             case 1:
                 $("#text1").html(lines[0]);
+                textNum = 241;
                 break;
             case 2:
                 $("#text1").html("Employee: " + "<br/>" + lines[1]);
@@ -666,8 +1040,8 @@ $(document).ready(function () {
                 break;
             case (secondSceneNum+1):
                 animateDiv();
-                $('#raindropDIV').hide(0);
-                $('#raindropGame').hide(0);
+                $('#raindropDIV').remove(0);
+                $('#raindropGame').remove(0);
                 $('#beginningPage').show(0);
 //                document.getElementById("beginningPage").style.backgroundImage = 'url("./images/testBack.jpeg")';
                 $("#text1").html("Employee: " + "<br/>" + lines2[0]);
@@ -988,6 +1362,14 @@ $(document).ready(function () {
             case 242:
                 $("#text1").html(userName + ":" + "<br/>" + lines3[42]);
                 break;
+            case 243:
+                $("#next").hide(0);
+                $('#overlay').animate({
+                    opacity: 1,
+                }, 1500, function () {
+                });
+                setTimeout(nextMinigame3, 1500);
+                break;
             case 1000:
                 animateDiv();
                 $('#raindropDIV').hide(0);
@@ -1067,51 +1449,18 @@ function nextMinigame2() {
         opacity: 0,
     }, 1000, function () {
     });
-    $("#text1").html("Turn off randomly turned on unused taps to conserve water.");
 }
 
-function updatePage() {
-    var text = document.getElementsByTagName("p");
-    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-    var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
-
-    for (let i = 0; i < text.length; i++) {
-        if (width > height) {
-            text[i].style.fontSize = height * 0.04 + "px";
-        } else {
-            text[i].style.fontSize = width * 0.04 + "px";
-        }
-    }
-    setTimeout(updatePage, 100);
-}
-updatePage();
-
-function updateTapPage() {
-    var taps = document.getElementsByClassName("tap");
-    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-    var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
-    var tapHeight = height * 0.19;
-    var tapWidth = width * 0.19;
-    for (let i = 0; i < taps.length; i++) {
-        if (width > height) {
-            taps[i].style.width = "auto";
-            taps[i].style.height = "19vh";
-        } else {
-            taps[i].style.height = "auto";
-            taps[i].style.width = "19%";
-        }
-    }
-    if (width > height) {
-        $("#tap2").css("left", (width / 2 - tapHeight / 2) + "px");
-        $("#tap5").css("left", (width / 2 - tapHeight / 2) + "px");
-        $("#tap8").css("left", (width / 2 - tapHeight / 2) + "px");
-    } else {
-        $("#tap2").css("left", (width / 2 - tapWidth / 2) + "px");
-        $("#tap5").css("left", (width / 2 - tapWidth / 2) + "px");
-        $("#tap8").css("left", (width / 2 - tapWidth / 2) + "px");
-    }
-    
-    setTimeout(updateTapPage, 10);
+function nextMinigame3() {
+    $('#beginningPage').hide(0);
+    $('#sliceDIV').show(0);
+    $('#sliceGame').show(0);
+    startSliceGame();
+    $('#overlay').animate({
+        opacity: 0,
+    }, 1000, function () {
+    });
+    $("#text1").html("Slice the objects wasting water.");
 }
 
 function nextEndGame() {
@@ -1138,3 +1487,20 @@ function nextEndGame2() {
         textNum = thirdSceneNum;
     }
 }
+
+
+function updatePage() {
+    var text = document.getElementsByTagName("p");
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+
+    for (let i = 0; i < text.length; i++) {
+        if (width > height) {
+            text[i].style.fontSize = height * 0.04 + "px";
+        } else {
+            text[i].style.fontSize = width * 0.04 + "px";
+        }
+    }
+    setTimeout(updatePage, 100);
+}
+updatePage();
